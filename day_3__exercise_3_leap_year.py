af025f2f8d6672930b99e85d29b329e24c669c32
Dominic Festa <Dominic@dfesta.org>
2022-11-18 18:16:00 -0700

    Update day_3__exercise_3_leap_year.py

--- day_3__exercise_3_leap_year.py
+++ day_3__exercise_3_leap_year.py
@@ -21,7 +21,21 @@ year = int(input("Which year do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-if (year % 4 == 0) and (year % 100 != 0) and (year % 400 != 0):
-    print(f"Leap year.")
+# solution 1
+# if (year % 4 == 0) and (year % 100 != 0):
+#     print(f"Leap year.")
+# elif (year % 4 == 0) and (year % 100 == 0) and (year % 400 == 0):
+#     print(f"Leap year.")
+# else:
+#     print(f"Not a leap year.")
+# solution answer
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            Print("Leap year.")
+        else:
+            print("Not a leap year.")
+    else:
+        print("Leap year.")
 else:
-    print(f"Not a leap year.")
+    print("Not a leap year.")
