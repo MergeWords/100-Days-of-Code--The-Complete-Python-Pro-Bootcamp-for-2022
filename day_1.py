ac67201d84ef090acee1c4fc01d04e0c18f65db5
Dominic Festa <Dominic@dfesta.org>
2022-11-18 19:38:38 -0700

    day 1-1

---
+++ day_1.py
@@ -0,0 +1,28 @@
+# day 1-1 question
+# Write a program in main.py that prints the same notes from the previous lesson using what you have learnt about the Python print function.
+# day 1-1 solution
+#Write your code below this line 👇
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+print("Hello " + input("What is your name?"))
+
+# variables
+name = input("What is your name?")
+print(name)
+
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+# Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+name = input("What is your name?")
+length = len(name)
+print(length)
+
+# day 1-4
--- day_3.py
+++ day_3.py
@@ -1,3 +1,49 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#Write your code below this line 👇
+
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if choice1 == "left":
+  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+  if choice2 == "wait":
+    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+    if choice3 == "red":
+      print("It's a room full of fire. Game Over.")
+    elif choice3 == "yellow":
+      print("You found the treasure! You Win!")
+    elif choice3 == "blue":
+      print("You enter a room of beasts. Game Over.")
+    else:
+      print("You chose a door that doesn't exist. Game Over.")
+  else:
+    print("You get attacked by an angry trout. Game Over.")
+else:
+  print("You fell into a hole. Game Over.")
+
+# day 3 start
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
@@ -6,6 +52,64 @@ if height >= 120:
 else:
   print("Sorry, you have to grow taller before you can ride.")
 
-# when you have = Assignment to variable
-# when you have == Check uequality of values
+# exercise 2
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
 
+#Write your code below this line 👇
+
+bmi = round(weight / height ** 2)
+if bmi < 18.5:
+  print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+  print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+  print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+  print(f"Your BMI is {bmi}, you are obese.")
+else:
+  print(f"Your BMI is {bmi}, you are clinically obese.")
+
+# Exercise 3 - Leap Year
+# Instructions
+# Write a program that works out whether if a given year is a leap year. A normal year has 365 days, leap years have 366, with an extra day in February. The reason why we have leap years is really fascinating, this video does it more justice:
+# https://www.youtube.com/watch?v=xX96xng7sAE
+# This is how you work out whether if a particular year is a leap year.
+# on every year that is evenly divisible by 4 
+# **except** every year that is evenly divisible by 100 
+# **unless** the year is also evenly divisible by 400
+# e.g. The year 2000:
+# 2000 ÷ 4 = 500 (Leap)
+# 2000 ÷ 100 = 20 (Not Leap)
+# 2000 ÷ 400 = 5 (Leap!)
+# So the year 2000 is a leap year.
+# But the year 2100 is not a leap year because:
+# 2100 ÷ 4 = 525 (Leap)
+# 2100 ÷ 100 = 21 (Not Leap)
+# 2100 ÷ 400 = 5.25 (Not Leap)
+
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+# solution 1
+# if (year % 4 == 0) and (year % 100 != 0):
+#     print(f"Leap year.")
+# elif (year % 4 == 0) and (year % 100 == 0) and (year % 400 == 0):
+#     print(f"Leap year.")
+# else:
+#     print(f"Not a leap year.")
+# solution answer
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not a leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not a leap year.")
